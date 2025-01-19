264d6e9c0d668143b2a852ecd39b0547d46aa493
fukugit <fukuma1234@gmail.com>
2025-01-19 19:02:40 +0900

    Set basePath to '/blog-nextjs' in next.config.js for consistent deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-const isProduction = process.env.NODE_ENV === 'production'; // 環境を判定
-const basePath = isProduction ? '/blog-nextjs' : ''; // 本番環境のみ basePath を設定
+// const isProduction = process.env.NODE_ENV === 'production'; // 環境を判定
+// const basePath = isProduction ? '/blog-nextjs' : ''; // 本番環境のみ basePath を設定
+
+const basePath = '/blog-nextjs'; // 本番環境のみ basePath を設定
 
 module.exports = {
   output: 'export',
