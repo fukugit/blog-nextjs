317ec1a198df8c83b3cc21194680caae527d5191
fukugit <fukuma1234@gmail.com>
2025-02-10 18:59:11 +0900

    Replace useRouter with environment variable for basePath in CoverImage component

--- src/app/_components/cover-image.tsx
+++ src/app/_components/cover-image.tsx
@@ -1,7 +1,6 @@
 import cn from "classnames";
 import Link from "next/link";
 import Image from "next/image";
-import { useRouter } from 'next/router';
 
 type Props = {
   title: string;
@@ -10,7 +9,7 @@ type Props = {
 };
 
 const CoverImage = ({ title, src, slug }: Props) => {
-  const { basePath } = useRouter(); // Next.js の `basePath` を取得
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
   const image = (
     <Image
       src={`${basePath}${src}`}
