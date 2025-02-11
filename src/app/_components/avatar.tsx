6a3c714e283603eb81555b30720755c751cbc382
fukugit <fukuma1234@gmail.com>
2025-02-11 09:08:14 +0900

    Update Avatar component to use basePath from environment variable for image source

--- src/app/_components/avatar.tsx
+++ src/app/_components/avatar.tsx
@@ -4,9 +4,11 @@ type Props = {
 };
 
 const Avatar = ({ name, picture }: Props) => {
+  // Get the base path from the environment variable of '/blog-nextjs'
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
   return (
     <div className="flex items-center">
-      <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
+      <img src={`${basePath}${picture}`} className="w-12 h-12 rounded-full mr-4" alt={name} />
       <div className="text-xl font-bold">{name}</div>
     </div>
   );
